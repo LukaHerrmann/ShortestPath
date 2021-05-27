d54054cd45deb897776855c2403ea7e198b510ab
LukaHerrmann <lukajherrmann@gmail.com>
2021-05-27 17:59:49 +0200

    constructor gemaakt

--- dijkstraShortestPath/src/vlucht.java
+++ dijkstraShortestPath/src/vlucht.java
@@ -1,2 +1,10 @@
 public class vlucht {
+    final int bagageverlieskans;
+    final double kosten;
+
+    public vlucht(int bagageverlieskans, double kosten) {
+        this.bagageverlieskans = bagageverlieskans;
+        this.kosten = kosten;
+    }
+
 }
