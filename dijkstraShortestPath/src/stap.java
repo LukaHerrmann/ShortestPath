5840e920df4d46b406669b2c68fecd1ffade9c71
LukaHerrmann <lukajherrmann@gmail.com>
2021-05-27 18:18:04 +0200

    constructor voor stap gemaakt

--- dijkstraShortestPath/src/stap.java
+++ dijkstraShortestPath/src/stap.java
@@ -1,2 +1,11 @@
 public class stap {
+    final Object reismethode;
+    final String beginpunt;
+    final String eindpunt;
+
+    public stap (Object reismethode, String beginpunt, String eindpunt) {
+        this.reismethode = reismethode;
+        this.beginpunt = beginpunt;
+        this.eindpunt = eindpunt;
+    }
 }
