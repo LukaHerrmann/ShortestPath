e2beeff5b9e289d7e9bc52f644f9c4693210fab6
LukaHerrmann <lukajherrmann@gmail.com>
2021-05-27 18:53:23 +0200

    getter voor alle mogelijke waardes voor reismethode

--- dijkstraShortestPath/src/stap.java
+++ dijkstraShortestPath/src/stap.java
@@ -8,4 +8,11 @@ public class stap {
         this.beginpunt = beginpunt;
         this.eindpunt = eindpunt;
     }
+
+    public double getWaarde() {
+        if (reismethode instanceof rit) {return ((rit) reismethode).getAfstand();}
+        if (reismethode instanceof treinrit) {return ((treinrit) reismethode).getTijdsduur();}
+        if (reismethode instanceof vlucht) {return ((vlucht) reismethode).getReisWaarde();}
+        else {return -1;}
+    }
 }
